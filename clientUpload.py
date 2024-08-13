1dec25d45453f91ca3398ef6e635421d8c671f27
Roni Bandini <bandini@gmail.com>
2024-08-13 18:02:59 -0300

    Add files via upload

--- clientUpload.py
+++ clientUpload.py
@@ -14,11 +14,12 @@ gui = GUI()
 
 np1 = NeoPixel(Pin((Pin.P13)),3)
 
-url = "http://192.168.1.100"
+url = "http://192.168.1.51"
 heartRateLimit=90
 
 os.system('clear')
 print("mmWave Heart and Respiration Monitor")
+print("Roni Bandini, August 2024")
 print("It will take some minutes to warm up")
 
 print("")
@@ -52,12 +53,14 @@ while True:
 
 
 	if int(myValues[0])>heartRateLimit:
-		np1.range_color(0,2,0x00FF00)
+		print("Red limit!")
+		np1.range_color(0,2,0xFF0000)
 		np1.brightness(128)
 	elif int(myValues[0])>0:
 		np1.range_color(0,2,0x00FF00)
 		np1.brightness(64)
 	elif int(myValues[0])==0:
+		print("Warming up...")
 		np1.range_color(0,2,0x0000FF)
 		np1.brightness(64)
 		
